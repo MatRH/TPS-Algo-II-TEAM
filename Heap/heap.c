e647e47f4ec2fffc39a3b7a91b9300afe8a86234
LisandroTorresetti <lisandrotorresetti@hotmail.com>
2019-06-09 23:55:49 -0300

    Subo algunas pruebas del heap, falta pulir ciertas cosas, pasa todas nuestras pruebas

--- Heap/heap.c
+++ Heap/heap.c
@@ -10,7 +10,7 @@ Corrector: Secchi, Ana
 #include <stdbool.h>
 #include <stddef.h>
 #include "heap.h"
-#define TAMANIO_INICIAL (size_t) 100
+#define TAMANIO_INICIAL (size_t) 50 //La cambie para probar mas facil las de volumen DEBUG
 
 struct heap{
 	void** datos;
@@ -22,10 +22,10 @@ struct heap{
 //Declaraciones funciones auxiliares
 void swap(void** arr, size_t pos1, size_t pos2);
 void downheap(void** arr, size_t pos, size_t tam, cmp_func_t cmp);
-void downheap_all(void** arr, size_t pos, size_t tam, cmp_func_t cmp);
 void** heapify(void** arr, cmp_func_t cmp, size_t n);
 void upheap(void** arr, size_t pos, size_t tam, cmp_func_t cmp);
 bool heap_redimensionar(heap_t* heap_viejo);
+void copiar_arreglo(void** origen, size_t largo, void** destino);
 /* *****************************************************************
  *                  IMPLEMENTACION PRIMITIVAS DEL HEAP
   * *****************************************************************/
@@ -46,10 +46,14 @@ heap_t *heap_crear(cmp_func_t cmp){
 heap_t *heap_crear_arr(void *arreglo[], size_t n, cmp_func_t cmp){
 	heap_t* heap = malloc(sizeof(heap_t));
 	if (!heap) return NULL;
-	//en la lista de correos dicen que hay que hacer una copia al parecer del arreglo
-	heap->datos = heapify(arreglo, cmp, n); // heapify devuelve un arreglo, aplicas heapify desde la pos (i/2) - 1, no se si estos parametros estan bien
+	size_t capacidad = 1 + n + n / 2; //Porque si te pasan n == 0 explota, nos garantiza que algo se guarda
+	void **arr = malloc(sizeof(void*) * capacidad);
+	if (!arr) return NULL;
+	copiar_arreglo(arreglo, n, arr); //Copia el arreglo pasado por parametro al arreglo de destino que hemos creado
+
+	heap->datos = heapify(arr, cmp, n); // heapify devuelve un arreglo, aplicas heapify desde la pos (i/2) - 1,
 	heap->cant_elem = n;
-	heap->capacidad = n + n/2; //para que tenga espacio libre extra, proporcional al asignado
+	heap->capacidad = capacidad; //para que tenga espacio libre extra, proporcional al asignado
 	heap->cmp = cmp;
 	return heap;
 
@@ -64,111 +68,103 @@ bool heap_esta_vacio(const heap_t *heap){
 }
 
 void *heap_ver_max(const heap_t *heap){
-	if(!heap) return NULL;
-	return (!heap->cant_elem) ? NULL : heap->datos[0];
+	return (!heap || !heap->cant_elem) ? NULL : heap->datos[0];
 }
 
 void heap_destruir(heap_t *heap, void destruir_elemento(void *e)){
 	if (destruir_elemento){ //La funcion de destruccion no es NULL
-		for (size_t i = 0; i < heap->cant_elem; i++){
-			destruir_elemento(heap->datos[i]);
-			free(heap->datos); //solo borro el array de datos si puedo borrar los datos
-		}
+		for (size_t i = 0; i < heap->cant_elem; i++) destruir_elemento(heap->datos[i]);
 	}
+	free(heap->datos);
 	free(heap);
 }
 
 bool heap_encolar(heap_t *heap, void *elem){
-	size_t cantidad = heap->cant_elem;
-	if (cantidad == heap->capacidad){
+	if (heap->cant_elem == heap->capacidad){
 		if(!heap_redimensionar(heap)) return false;
 	}
-	(heap->datos)[cantidad] = elem;
-	heap->cant_elem++;
-	upheap(heap->datos, cantidad, heap->capacidad, heap->cmp);
+	heap->datos[heap->cant_elem] = elem; 
+	upheap(heap->datos, heap->cant_elem++, heap->capacidad, heap->cmp);
 	return true;
 }
 
 void *heap_desencolar(heap_t *heap){
-	if(!heap || !heap->cant_elem) return NULL;
+	if(!heap || !heap->cant_elem) return NULL; 
 	void** arr = heap->datos;
 	void* tope = arr[0];
 	//cambio la raiz por el último
-	swap(arr, 0, heap->cant_elem);
-	heap->cant_elem--; //el próximo que encole va a pisar al viejo
+	swap(arr, 0, --heap->cant_elem);
 	downheap(arr, 0, heap->cant_elem, heap->cmp);
 	return tope;
 }
 
 void heap_sort(void *elementos[], size_t cant, cmp_func_t cmp){
-	elementos = heapify(elementos, cmp, cant);
+	if (cant < 1) return;  //Porque si esta vacio o tiene un elemento ya esta ordenado
+	elementos = heapify(elementos, cmp, cant); //Primer paso, darle forma de heap al arreglo
+	//Segundo paso, iteramos y swapeamos la raiz con el "ultimo relativo"
+	size_t ultimo_relativo = cant - 1;
+	while (ultimo_relativo){
+		swap(elementos, 0, ultimo_relativo); //Intercambiamos el mayor con el ultimo elemento del arreglo
+		downheap(elementos, 0, --ultimo_relativo, cmp); //Reacomodas el heap, pero antes actualizas ultimo relativo
+	}
+
 }
 
 //Funciones auxiliares
 void** heapify(void** arr, cmp_func_t cmp, size_t n){
 	//Aplicas downheap desde (n/2) - 1 hacia arriba
 	size_t pos = (n / 2) - 1;
-	downheap_all(arr, pos, n, cmp);
+	downheap(arr, pos, n, cmp);
 	return arr;
 }
 
-bool heap_redimensionar(heap_t* heap_viejo){ //O(n)
-	heap_t* heap_nuevo = heap_crear_arr(heap_viejo->datos, heap_viejo->cant_elem, heap_viejo->cmp);
-	if (!heap_nuevo) return false;
-	heap_viejo->datos = heap_nuevo->datos; //me llevo el arreglo de datos
-	heap_viejo->capacidad = heap_nuevo->capacidad;//actualizo la capacidad
-	heap_destruir(heap_nuevo, NULL); //destruyo la estructura pero no los datos
-	return true;
-}
-
-void downheap_all(void** arr, size_t pos, size_t tam, cmp_func_t cmp){
-	if (pos >= tam) return;
-	size_t max = pos;
-	size_t hijo_izq = 2 * pos + 1;
-	size_t hijo_der = 2 * pos + 2;
-
-	if (hijo_izq < tam && cmp(arr[hijo_izq], arr[max]) > 0) max = hijo_izq;
-
-	if (hijo_der < tam && cmp(arr[hijo_der], arr[max]) > 0) max = hijo_der;
-	//Veo si tengo que swapear
-	if (max != pos){
-	 	max = (cmp(arr[hijo_izq], arr[hijo_der]) > 0) ? hijo_der : hijo_izq; //me quedo con el hijo más grande
-		swap(arr, pos, max);
-		downheap_all(arr, max, tam, cmp); //hago downheap desde donde deje al padre
+bool heap_redimensionar(heap_t* heap){ //O(n)
+	heap->capacidad *= 2;
+	void** datos_nuevo = realloc(heap->datos, heap->capacidad * sizeof(void*));  //hace la magia
+	if (!datos_nuevo){
+		free(heap); //Libero la memoria pedida para heap cuando lo cree
+		return false; //Los datos se liberan solos al fallar
 	}
-	if (pos == 0) return;
-	downheap_all(arr, --pos, tam, cmp);
+	heap->datos = datos_nuevo;
+	return true;
 }
 
 void downheap(void** arr, size_t pos, size_t tam, cmp_func_t cmp){
 	if (pos >= tam) return;
-	size_t max = pos;
-	size_t hijo_izq = 2 * pos + 1;
-	size_t hijo_der = 2 * pos + 2;
+	size_t max = pos; 
+	size_t hijo_izq = 2 * pos + 1;  
+	size_t hijo_der = 2 * pos + 2; 
 
 	if (hijo_izq < tam && cmp(arr[hijo_izq], arr[max]) > 0) max = hijo_izq;
 
 	if (hijo_der < tam && cmp(arr[hijo_der], arr[max]) > 0) max = hijo_der;
 	//Veo si tengo que swapear
 	if (max != pos){
-	 	max = (cmp(arr[hijo_izq], arr[hijo_der]) > 0) ? hijo_der : hijo_izq; //me quedo con el hijo más grande
 		swap(arr, pos, max);
 		downheap(arr, max, tam, cmp); //hago downheap desde donde deje al padre
 	}
+	if (pos == 0) return;
+	downheap(arr, --pos, tam, cmp);
 }
 
 void upheap(void** arr, size_t pos, size_t tam, cmp_func_t cmp){
 	if (pos == 0 || pos > tam) return;
-	size_t padre = (pos -1) /2;
+	size_t padre = (pos - 1 == 1) ? 1 : (pos - 1) / 2; //Porque cuando pasas 3 cosas, (2-1) / 2 da cero y esta mal, tenes que comparar con padre 1 al que esta en la pos 2 
 
-	if (cmp(arr[padre], arr[pos]) > 0){
+	if (cmp(arr[padre], arr[pos]) < 0){
 		swap(arr, pos, padre);
-		upheap(arr, padre, tam, cmp); //hago upheap desde donde deje al padre
+		upheap(arr, padre, tam, cmp); 
 	}
-}
+} 
 
 void swap(void** arr, size_t pos1, size_t pos2){
 	void* aux = arr[pos1];
 	arr[pos1] = arr[pos2]; //Cambio la posicion del primero
 	arr[pos2] = aux; //cambio la posicion del segundo
+}
+
+void copiar_arreglo(void** origen, size_t largo, void** destino){
+	/*Copia los datos del arreglo origen en el arreglo destino*/
+	if (!largo) destino[0] = NULL;
+	for (size_t i = 0; i < largo; i++) destino[i] = origen[i];
 }
--- Heap/pruebas_alumno.c
+++ Heap/pruebas_alumno.c
@@ -13,7 +13,6 @@ Corrector: Secchi, Ana
 #include <string.h>
 #include <unistd.h> //para el ssize_t
 
-typedef int (*cmp_func_t) (const void *a, const void *b);
 bool cond_inic_heap(heap_t* heap){
 	return !heap_cantidad(heap) && heap != NULL && heap_esta_vacio(heap);
 }
@@ -43,7 +42,7 @@ static void pruebas_heap_vacio(){
 	heap_destruir(heap, NULL);
 	print_test("El heap se destruyo correctamente", true);
 
-	void* arr_vacio[] = {}; //Arreglo vacio
+	void* arr_vacio[] = {NULL}; //Arreglo vacio
 	heap_t* heap_arr = heap_crear_arr(arr_vacio, 0, heap_numcmp);
 	print_test("El heap_arr cumple las condiciones iniciales", cond_inic_heap_arr(heap_arr, 0));
 	print_test("Ver max de arreglo vacio es NULL", !heap_ver_max(heap_arr));
@@ -53,56 +52,56 @@ static void pruebas_heap_vacio(){
 }
 
 static void pruebas_heap_encolar(){
-	printf("INICIO PRUEBAS HEAP ENCOLAR\n");
+	printf("\nINICIO PRUEBAS HEAP ENCOLAR\n");
 
 	heap_t* heap = heap_crear(heap_strcmp);
 	print_test("El heap cumple las condiciones iniciales", cond_inic_heap(heap));
 
-	char* cad1 = "perro", cad2 = "gato", cad3 = "zorro";
+	char *cad1 = "perro", *cad2 = "gato", *cad3 = "zorro";
+
 	print_test("Heap encolar 'perro'", heap_encolar(heap, cad1));
 	print_test("La cantidad de elementos es 1", heap_cantidad(heap) == 1);
 	print_test("Heap ver maximo es 'perro'", heap_ver_max(heap) == cad1);
 	print_test("El heap no esta vacio", !heap_esta_vacio(heap));
 
 	print_test("Heap encolar 'gato'", heap_encolar(heap, cad2));
-	print_test("La cantidad de elementos es 1", heap_cantidad(heap) == 2);
+	print_test("La cantidad de elementos es 2", heap_cantidad(heap) == 2);
 	print_test("Heap ver maximo sigue siendo 'perro'", heap_ver_max(heap) == cad1);
 	print_test("El heap no esta vacio", !heap_esta_vacio(heap));
 
 	print_test("Heap encolar 'zorro'", heap_encolar(heap, cad3));
-	print_test("La cantidad de elementos es 1", heap_cantidad(heap) == 3);
+	print_test("La cantidad de elementos es 3", heap_cantidad(heap) == 3);
 	print_test("Heap ver maximo es 'zorro'", heap_ver_max(heap) == cad3);
-	print_test("El heap no esta vacio", !heap_esta_vacio(heap));
+	print_test("El heap no esta vacio", !heap_esta_vacio(heap)); //hasta aca anda
 
 	print_test("Heap desencolar devuelve 'zorro'", heap_desencolar(heap) == cad3);
 	print_test("El heap no esta vacio", !heap_esta_vacio(heap));
 	print_test("Heap desencolar devuelve 'perro'", heap_desencolar(heap) == cad1);
 	print_test("El heap no esta vacio", !heap_esta_vacio(heap));
 	print_test("Heap desencolar devuelve 'gato'", heap_desencolar(heap) == cad2);
-	print_test("El heap no esta vacio", !heap_esta_vacio(heap));
-	print_test("Heap desencolar devuelve NULL", !heap_desencolar(heap));
 	print_test("El heap esta vacio", heap_esta_vacio(heap));
+	print_test("Heap desencolar devuelve NULL", !heap_desencolar(heap));
 	heap_destruir(heap, NULL);
 	print_test("El heap se destruyo correctamente", true);
 }
 
-static void prueba_heap_arr(){
+/*static void prueba_heap_arr(){
 	printf("INICIO DE PRUEBAS DE HEAP ARR\n");
 
 	void* arr_vacio[] = {};
 	heap_t* heap_arr = heap_crear_arr(arr_vacio, 0, heap_numcmp);
 	print_test("El heap_arr cumple las condiciones iniciales", cond_inic_heap_arr(heap_arr, 0));
 
-}
+}*/
 
 static void prueba_heap_destruir(){
-	printf("INICIO PRUEBAS DE DESTRUCCION DEL HEAP\n");
+	printf("\nINICIO PRUEBAS DE DESTRUCCION DEL HEAP\n");
 	printf("\nPrueba 1: destruir pasandole parametro NULL\n");
 
 	heap_t* heap = heap_crear(heap_strcmp);
 	print_test("El heap cumple las condiciones iniciales", cond_inic_heap(heap));
 
-	char* cad1 = "toro", cad2 = "zorro", cad3 = "rinoceronte";
+	char *cad1 = "toro", *cad2 = "zorro", *cad3 = "rinoceronte";
 
 	print_test("Heap encolar 'toro'", heap_encolar(heap, cad1));
 	print_test("Heap encolar 'zorro'", heap_encolar(heap, cad2));
@@ -137,11 +136,70 @@ static void prueba_heap_destruir(){
 	print_test("Se destruyo correctamente el heap con el parametro free", true);
 }
 
-static void prueba_heapsort(){
+/*static void prueba_heapsort(){
 	printf("INICIO PRUEBA HEAPSORT\n");
+}*/
 
+
+
+static void prueba_heap_volumen(size_t largo)
+{
+    printf("INICIO PRUEBAS HEAP VOLÚMEN\n");
+    heap_t* heap = heap_crear(heap_strcmp);
+
+    const size_t largo_clave = 10;
+    char** claves = malloc(sizeof(char*) * largo);
+
+    // Inserta 'largo' parejas en el abb
+    bool ok = true;
+    for (unsigned i = 0; i < largo; i++) {
+        claves[i] = malloc(sizeof(char) * largo_clave);
+        sprintf(claves[i], "%08d", i);
+    }
+    //Encolo las claves
+    for (unsigned i = 0; i < largo; i++){
+        ok = heap_encolar(heap, claves[i]);
+        if (!ok){
+        	print_test("Falla encolando muchos elementos", ok);
+        	break;
+        }
+    }
+
+    print_test("Prueba heap almacenar muchos elementos", ok);
+    print_test("Prueba heap la cantidad de elementos es correcta", heap_cantidad(heap) == largo);
+    print_test("Prueba heap ver maximo es corecta", heap_ver_max(heap) == claves[largo - 1]);
+
+    // Verifica que desencole en el orden correcto
+    for (size_t i = 0, j = largo - 1; i < largo; i++, j--) {
+        char* desencolado = heap_desencolar(heap);
+        ok = desencolado == claves[j]; //quizas falla
+        if (!ok){
+          printf("Valor esperado : %s\n", claves[j]);
+          printf("Valor obtenido : %s\n", desencolado);
+          break;
+        };
+    }
+
+    print_test("Prueba heap desecolar muchos elementos", ok);
+    printf("La cantidad de elementos es supuestamente %ld\n", heap_cantidad(heap));
+    print_test("Prueba heap la cantidad de elementos es 0", !heap_cantidad(heap));
+    print_test("Ver max devuelve NULL", heap_ver_max(heap) == NULL);
+    print_test("Heap desencolar devuelve NULL", heap_desencolar(heap) == NULL);
+
+
+    // Destruye el abb y crea uno nuevo que sí libera
+    heap_destruir(heap, NULL);
+    //Libero la memoria pedida para las claves y valores
+    for (size_t i = 0; i < largo; i ++) free(claves[i]);
+    free(claves);
 }
 
-static void prueba_volumen(size_t volumen){
-	
+void pruebas_heap_alumno()
+{
+    /* Ejecuta todas las pruebas unitarias. */
+    pruebas_heap_vacio();
+    pruebas_heap_encolar();
+    prueba_heap_destruir();
+    prueba_heap_volumen(1000);
+    //prueba_heapsort();
 }
