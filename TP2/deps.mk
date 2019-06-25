ba646bbd3e0afb2e0140c4f9a25f547f0d523259
MatRH <ruizhuidobro.matias@gmail.com>
2019-06-25 12:31:47 -0300

    Avanzada la funcion de ordenamiento

---
+++ TP2/deps.mk
@@ -0,0 +1,3 @@
+procesar_tweets: procesar_tweets.o count_min_sketch.o strutil.o tupla.o
+procesar_usuarios: procesar_usuarios.o hash.o strutil.o lista.o tupla.o
+
--- TP2/tupla.c
+++ TP2/tupla.c
@@ -44,43 +44,79 @@ void tupla_destruir(tupla_t* tupla){
 }
 
 /*ORDENA IN PLACE un arreglo de tuplas, primero segun su frecuencia y luego
-alfabeticamente
-void ordernar_tuplas(tupla_t** tuplas, size_t len){
-	size_t max_frec = 0;.
+alfabeticamente*/
+/*
+1) Ordeno el arreglo con radix sort,	O(len)
+2) Recorro una vez el arreglo para encontrar el rango de frecuencias O(len)
+3) Creo (Max-min) baldes, O(M-m)
+4) Recorro una vez el arreglo y separo cada tupla en su balde correspondiente
+según su frecuencia						O(len)
+
+*/
+tuplas_t** ordenar_abc(tuplas_t** tuplas,size_t len);
+
+tuplas_t** ordernar_tuplas(tupla_t** tuplas, size_t len){
+	size_t max_frec = 0;
+	size_t min_frec = tupla_frec(tuplas[0]);
 	tupla_t tupla;
-	size_t capacidad = len/2;
-	size_t* usuarios_por_frecuencia = malloc(sizeof(size_t)*capacidad);
-	bool todo_ok = true;
+	size_t pos_baldes;
 	size_t frec;
-	for (int i;todo_ok && i < len, i++){//recorro una vez para ver el rango de los datos
-		tupla = tuplas[i];
-		size_t frec = tupla_frec(tupla);
-		if(frec > max_frec) max_frec = frec;
-		if(frec > capacidad){
-			capacidad = capacidad*2;
-			usuarios_por_frecuencia = realloc(usuarios_por_frecuencia, capacidad);
-			if(!usuarios_por_frecuencia) todo_ok = false;
-		}
-		usuarios_por_frecuencia[frec]++;
-	}
-	if(!todo_ok return);
-	void*** frecuencias = malloc(sizeof(void*)*max_frec);
-	for (int n; n <= max_frec; n++){//creo los baldes
-		if (!frecuencias[n]){
-			tuplas_t** balde = malloc(sizeof(void*)*usuarios_por_frecuencia[n]);
-			frecuencias[n] = balde;
-		}
-	}
-	for(int i; i<len; i++){ //ordeno las tuplas por frecuencias en sus baldes
+	tuplas = ordenar_abc(tuplas, len);
+	if (!tuplas) return NULL;
+
+	for (size_t i = 0; i < len; i++){ //OBTENGO MAXIMO Y MINIMO	O(len)
 		tupla = tuplas[i];
 		frec = tupla_frec(tupla);
-		size_t n = 0;
-		while(frecuencias[frec][n]){
-			n++;
-		}
-		frecuencias[frec][n] = tupla;
+		if (frec < min_frec) min_frec = frec;
+		if (frec > max_frec) max_frec = frec;
 	}
-	for (int n; n<= max_frec; n++){
+	size_t rango = max_frec - min_frec;
 
+	void* baldes = malloc(sizeof(void*)*rango);
+	size_t tams[rango];
+
+	for (size_t i = 0; i < len; i++){//BUSCO EL LARGO DE CADA BALDE
+		tupla = tuplas[i];
+		frec = tupla_frec(tupla);
+		pos_baldes = frec - min_frec;
+		tams[pos_baldes]++;
 	}
-}*/
+
+	for (size_t i = 0; i < rango; i++){
+		void* balde = malloc(sizeof(void*)*tams[i]);//creo cada balde con su tamaño correspondiente
+		baldes[i] = balde;
+		tams[i] = 0; //inicializo en 0 el arreglo de tamaños para reutilizarlo más adelante
+	}
+
+	//AHORA TENGO UN ARREGLO DE BALDES, DONDE CADA BALDE TIENE SUFICIENTE ESPACIO PARA GUARDAR A TODOS LOS DE SU FRECUENCIA
+	size_t pos_balde;
+	for (size_t i = 0; i < len; i++ ){//SEPARO CADA TUPLA SEGÚN SU FRECUENCIA, RESPETANDO EL ORDEN RELATIVO QUE TENIAN
+		tupla = tuplas[i];
+		frec = tupla_frec(tupla);
+		pos_baldes = frec - min_frec; //asi min es el 0, min +1 en el 1 elemento etc
+		pos_balde = tams[pos_baldes]; //posicion dentro del arreglo de su balde correspondiente
+		baldes[pos_baldes][pos_balde] = tupla;//guardo la tupla en su balde correspondiente, en su posicion correspondiente
+		tams[pos_baldes]++;//para saber donde va la siguiente tupla en su correspondiente balde
+	}
+
+	//AHORA TENGO BALDES SEGÚN FRECUENCIA, DONDE EN CADA BALDE LAS TUPLAS ESTÁN ORDENADAS ALFABÉTICAMENTE
+	tuplas_t** ordenado = malloc(sizeof(void*)*len);
+	size_t pos = 0;
+	for (size_t balde_actual = 0; balde_actual < rango; balde_actual++){
+		pos_balde = 0;
+		while ( tams[balde_actual]){
+			pos_balde++; //posicion en el balde actual
+			ordenado[pos] = baldes[balde_actual][pos_balde] //pongo la tupla en su posicion correspondiente en el arreglo
+			pos++; //posicion de la tupla en el arreglo ordenado
+			tams[balde_actual]--;
+		}
+		free(baldes[balde_actual]);//libero el balde vacio
+	}
+	free(baldes);//libero el arreglo de baldes
+	return (ordenado);
+}
+
+
+tuplas_t** ordenar_abc(tuplas_t** tuplas,size_t len){
+
+}
